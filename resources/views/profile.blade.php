357a3c781b69cd93ee4fc223f995c508f34fbe98
tyukhtei <77382866+tyukhtei@users.noreply.github.com>
2023-04-08 23:31:19 +0500

    изменил форму

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -2,30 +2,35 @@
 @section('title','Авторизация')
 @section('content')
 <main>
+    <div class="addData">
     <form action="" method="post">
+        <label for="WorkSpace">Рабочая область:</label>
         <input name="WorkSpace" list="WorkSpaceList" id="WorkSpace">
-        
         <datalist id="WorkSpaceList">
             <option value="WorkSpace_1">Область работы 1</option>
             <option value="WorkSpace_2">Область работы 2</option>
         </datalist>
 
+        <label for="City">Город:</label>
         <input name="City" list="CityList" id="City">
         <datalist id="CityList">
             <option value="Tyumen">Тюмень</option>
             <option value="Moskow">Москва</option>
         </datalist>
 
+        <label for="Brand">Бренд:</label>
         <input name="Brand" list="BrandList" id="Brand">
         <datalist id="BrandList">
             <option value="Parusa">Parusa</option>
         </datalist>
         
+        <label for="CoffeeHouse">Кофейня:</label>
         <input name="CoffeeHouse" list="CoffeeHouseList" id="CoffeeHouse">
         <datalist id="CoffeeHouseList">
             <option value="CoffeeHouse">Кофейня</option>
         </datalist>
-
+        
+        <label for="NomenclatureСategory">Категория номенклатуры:</label>
         <input name="NomenclatureСategory" list="NomenclatureСategoryList" id="NomenclatureСategory">
         <datalist id="NomenclatureСategoryList">
             <option value="AlternativeMilk">Альтернативное молоко</option>
@@ -47,12 +52,16 @@
             <option value="Eggs">Яйца</option>
         </datalist>
         
+        <label for="ItemNomenclatureIiko">Элемент номенклатуры iiko:</label>
         <input name="ItemNomenclatureIiko" id="ItemNomenclatureIiko">
 
+        <label for="UnitMeasurementPackageSize">Единица измерения с учетом размера упаковки:</label>
         <input name="UnitMeasurementPackageSize" id="UnitMeasurementPackageSize">
         
+        <label for="TargetPrice">Целевая цена:</label>
         <input name="TargetPrice" id="TargetPrice">
 
+        <label for="Supplier">Поставщик:</label>
         <input name="Supplier" list="SupplierList" id="Supplier">
         <datalist id="SupplierList">
             <option value="0">0</option>
@@ -63,8 +72,10 @@
             <option value="TyumenMilk">ТюменьМолоко</option>
         </datalist>
         
+        <label for="ItemNomenclature">Элемент номенклатуры:</label>
         <input name="ItemNomenclature" id="ItemNomenclature">
 
+        <label for="ABC">ABC:</label>
         <input name="ABC" list="ABCList" id="ABC">
         <datalist id="ABCList">
             <option value="15%">15%</option>
@@ -72,45 +83,57 @@
             <option value="80%">80%</option>
         </datalist>
 
+        <label for="LeftoversVersionManagement">Осатки Версия Управ-х, ед изм.:</label>
         <input name="LeftoversVersionManagement" id="LeftoversVersionManagement">
 
+        <label for="RemnantsOfIiko">Осатки IIKO, ед изм.:</label>
         <input name="RemnantsOfIiko" id="RemnantsOfIiko">
 
+        <label for="Predict">Прогноз:</label>
         <input name="Predict" id="Predict">
 
+        <label for="PredictEdIzm">Прогноз, ед.изм.:</label>
         <input name="PredictEdIzm" id="PredictEdIzm">
 
+        <label for="Order">Заказ с учетом остков, ед. изм:</label>
         <input name="Order" id="Order">
 
+        <label for="CorrectionManager">Корретировка управляющего:</label>
         <input name="CorrectionManager" id="CorrectionManager">
 
+        <label for="ManagerComment">Комментарий управляющего:</label>
         <input name="ManagerComment" id="ManagerComment">
 
+        <label for="OrderAdjustmentManager">Заказ с учетом корректировки управляющего:</label>
         <input name="OrderAdjustmentManager" id="OrderAdjustmentManager">
 
+        <label for="Monday">Понедельник:</label>
         <input name="Monday" id="Monday">
 
+        <label for="Tuesday">Вторник:</label>
         <input name="Tuesday" id="Tuesday">
         
+        <label for="Wednesday">Среда:</label>
         <input name="Wednesday" id="Wednesday">
         
+        <label for="Thursday">Четверг:</label>
         <input name="Thursday" id="Thursday">
 
+        <label for="Friday">Пятница:</label>
         <input name="Friday" id="Friday">
         
+        <label for="Saturday">Суббота:</label>
         <input name="Saturday" id="Saturday">
         
+        <label for="Sunday">Воскресение:</label>
         <input name="Sunday" id="Sunday">
         
+        <label for="Check">Проверка:</label>
         <input name="Check" id="Check">
 
-
-        
-
-
-
-
-        
+        <button>Отправить</button>
     </form>
+    </div>
+    
 </main>
 @endsection
