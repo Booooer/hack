b04f2f25dbf2850de7cbe29f2b93f0caccde01d1
Booooer <tsurkan.maksim2016@gmail.com>
2023-04-09 00:44:46 +0500

    добавил товары

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    public function add(){
+        $data = json_decode(file_get_contents("php://input"));
+
+        $add = Product::create([
+            'categorya_3' => "Товары",
+            'categorya_nomenklatyri' => $data[1],
+            'element_nomenklatyri' => $data[0],
+            'itog' => $data[2],
+        ]);
+
+        if ($add) {
+            return json_encode("Товар добавлен");
+        }
+        return json_encode("Пошёл нахуй");
+    }
+}
